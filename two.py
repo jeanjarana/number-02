431ac5687dec5ce1104fdc82c7113c6511b2b1f0
Jean Jara <90890274+jeanjarana@users.noreply.github.com>
2022-04-29 14:12:58 +0800

    Create two.py

---
+++ two.py
@@ -0,0 +1,13 @@
+print("hi !!!")
+myint = 10
+print("this is my lucky number:", myint)
+print("when i am at my luckiest, the number doubles ! :", myint*2)
+
+print("is yours the same as mine?")
+yours = int(input("please type it in: "))
+if myint > yours :
+    print("ohh, yours is lower")
+elif myint == yours :
+    print("i see, we have the same lucky number")
+else :
+    print("hmm, higher it is.")
